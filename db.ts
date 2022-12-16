1343ed572dca148c7d3a20cae31fdcd92eb93a8a
Danilo Brun <danilosbrun@gmail.com>
2022-12-16 11:40:55 -0300

    config: ajusta URI de conexao

--- db.ts
+++ db.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("mysql://root:Cifra@2021@localhost:3306", {
+const sequelize = new Sequelize("mysql://root:cifra@2021@localhost:3306", {
   dialect: "mssql",
 });
 
-console.log(sequelize);
+// export instance of sequelize conection
+export default sequelize;
