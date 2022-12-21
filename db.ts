ee081394bb50a8b66d8058c69ae9c889e9feef5e
Danilo Brun <danilosbrun@gmail.com>
2022-12-20 21:30:53 -0300

    add: configura env para ambiente de dev e conexao e ajsuta conexão o ao banco.

--- db.ts
+++ db.ts
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+require("dotenv").config();
 
-const sequelize = new Sequelize("mysql://root:cifra@2021@localhost:3306", {
-  dialect: "mssql",
-});
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD_MAC = process.env.DB_PASSWORD_MAC;
+
+const sequelize = new Sequelize(
+  `mysql://${DB_USER}:${DB_PASSWORD_MAC}@localhost:3306`,
+  {
+    dialect: "mssql",
+  }
+);
 
 // export instance of sequelize conection
 export default sequelize;
